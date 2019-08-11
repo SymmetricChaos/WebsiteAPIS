94d1d2bb87220fb7dd09869960a39b8d3b9d7689
SymmetricChaos <ajfraebel@gmail.com>
2019-08-11 00:34:42 -0400

    Create scryfall.py

---
+++ ScryfallMTG/scryfall.py
@@ -0,0 +1,28 @@
+#https://api.scryfall.com
+
+import requests
+import json
+
+def SFrequest(S):
+    sfurl = "https://api.scryfall.com/"
+    resp = requests.get(sfurl+S)
+    J = json.loads(resp.content.decode('utf-8'))
+    if "error" in J:
+        raise Exception(f"Error {J['code']} {J['error']}\nRequest: {sfurl+S}")
+    return J
+
+#a = SFrequest("sets/aer")
+#print(a)
+
+
+a = SFrequest("cards?page=3")
+for i in a['data'][0].items():
+    print(i)
+print()
+print(a['data'][0]['layout'])
+print(a['data'][0]['object'])
+print(a['data'][0]['mana_cost'])
+print(a['data'][0]['cmc'])
+print(a['data'][0]['reprint'])
+print(a['data'][0]['name'])
+print(a['data'][0]['set'])
